f9a8ad2868f0c6ac60908f080fd353fd7a678f9e
Henrique L Alves <henriquelalves@gmail.com>
2018-02-09 11:06:24 -0200

    Fixed identation

--- src/states/menu.js
+++ src/states/menu.js
@@ -1,20 +1,20 @@
 class Menu extends Phaser.State {
 
     constructor() {
-	super();
+        super();
     }
-    
+
     create() {
-	this.background = this.game.add.image(this.game.width/2.0,this.game.height/2.0,'background');
-	this.background.anchor.set(0.5);
+        this.background = this.game.add.image(this.game.width / 2.0, this.game.height / 2.0, 'background');
+        this.background.anchor.set(0.5);
 
-	this.input.onDown.add(this.startGame, this);
+        this.input.onDown.add(this.startGame, this);
     }
 
-    update() {}
+    update() { }
 
-    startGame () {
-	// this.game.state.start('game');
+    startGame() {
+        // this.game.state.start('game');
     }
 }
 
